b326939811fc7fbbe4ea6c2ef92df924171528f8
Robert.Yang <ysb05222@naver.com>
2021-06-27 17:04:16 +0900

    @observable (by mobx)
    @observable (by mobx)

--- mobx-start/src/index.js
+++ mobx-start/src/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { autorun, makeObservable, observable } from "mobx";
+
+const isLogin = observable(true);
+
+const person = observable({
+  name: "Sungbin",
+  age: 26,
+});
+
+class PersonStore {
+  @observable
+  name = "Sungbin";
+
+  @observable
+  age = 26;
+
+  constructor() {
+    makeObservable(this);
+  }
+}
+
+const personStore = new PersonStore();
+
+autorun(() => {
+  console.log(isLogin.get());
+  console.log(person.age);
+  console.log(personStore.age);
+});
+
+isLogin.set(false);
+
+person.age = 22;
+
+personStore.age = 28;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
